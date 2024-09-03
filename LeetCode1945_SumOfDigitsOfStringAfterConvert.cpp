b5605a883f5d39d991236821ec067eca4a248e46
Ankita Mandal <74826116+Ankita27052002@users.noreply.github.com>
2024-09-03 19:57:39 +0530

    Create LeetCode1945_SumOfDigitsOfStringAfterConvert.cpp
    LeetCode Easy Problem Sum of Digits of String After Convert
    solution in cpp.

---
+++ LeetCode1945_SumOfDigitsOfStringAfterConvert.cpp
@@ -0,0 +1,25 @@
+// LeetCode Problem 1945 : Sum of Digits of String After Convert (Easy)
+
+// T.C : O(n)
+// S.C : O(1)
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string number = "";
+        for(char x: s){
+            number += to_string(x - 'a'+1);
+        }
+
+        while(k > 0){
+            int temp = 0;
+            for(char x: number){
+                temp += x - '0';
+            }
+            number = to_string(temp);
+            k--;
+        }
+
+        return stoi(number);
+    }
+};
